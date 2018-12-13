56dc5f9a1ee4ee2bc3e02ee67246f45e7690db1f
Stefano Zampini <stefano.zampini@gmail.com>
2018-12-13 17:15:28 +0300

    TSOPT: remove struct from private header

--- include/petscopt/private/tsoptimpl.h
+++ include/petscopt/private/tsoptimpl.h
@@ -24,22 +24,4 @@ PETSC_INTERN PetscErrorCode TSOptEvalGradientIC(TSOpt,PetscReal,Vec,Vec,Mat*,Mat
 PETSC_INTERN PetscErrorCode TSOptEvalHessianDAE(TSOpt,PetscInt,PetscInt,PetscReal,Vec,Vec,Vec,Vec,Vec,Vec);
 PETSC_INTERN PetscErrorCode TSOptEvalHessianIC(TSOpt,PetscInt,PetscInt,PetscReal,Vec,Vec,Vec,Vec,Vec);
 
-struct _TSOpt {
-  TS                ts;         /* pointer to the parent TS (not reference counted) */
-  TSEvalGradientIC  Ggrad;      /* compute the IC Jacobian terms G_m(x(t0),m) and G_x(x(t0),m) */
-  Mat               G_x;
-  Mat               G_m;
-  void              *Ggrad_ctx;
-  TSEvalHessianIC   HG[2][2];   /* compute the IC Hessian terms G_xx, G_xm, G_mx and G_mm */
-  void              *HGctx;
-  TSEvalGradientDAE F_m_f;      /* compute the DAE Jacobian term F_m(x,x_t,t;m) */
-  Mat               F_m;
-  Mat               adjF_m;
-  void              *F_m_ctx;
-  TSEvalHessianDAE  HF[3][3];   /* compute the DAE Hessian terms F_{x|x_t|m}{x|x_t|m} */
-  void              *HFctx;
-  PetscErrorCode    (*setupfromdesign)(TS,Vec,Vec,void*);
-  void              *setupfromdesignctx;
-};
-
 #endif
--- src/ts/tsopt.c
+++ src/ts/tsopt.c
@@ -8,6 +8,24 @@ PetscLogEvent TSOPT_Opt_Eval_Hess_IC  = 0;
 PetscLogEvent TSOPT_Opt_SetUp         = 0;
 PetscBool TSOPT_OptPackageInitialized = PETSC_FALSE;
 
+struct _TSOpt {
+  TS                ts;         /* pointer to the parent TS (not reference counted) */
+  TSEvalGradientIC  Ggrad;      /* compute the IC Jacobian terms G_m(x(t0),m) and G_x(x(t0),m) */
+  Mat               G_x;
+  Mat               G_m;
+  void              *Ggrad_ctx;
+  TSEvalHessianIC   HG[2][2];   /* compute the IC Hessian terms G_xx, G_xm, G_mx and G_mm */
+  void              *HGctx;
+  TSEvalGradientDAE F_m_f;      /* compute the DAE Jacobian term F_m(x,x_t,t;m) */
+  Mat               F_m;
+  Mat               adjF_m;
+  void              *F_m_ctx;
+  TSEvalHessianDAE  HF[3][3];   /* compute the DAE Hessian terms F_{x|x_t|m}{x|x_t|m} */
+  void              *HFctx;
+  PetscErrorCode    (*setupfromdesign)(TS,Vec,Vec,void*);
+  void              *setupfromdesignctx;
+};
+
 static PetscErrorCode TSOptDestroy_Private(void *ptr)
 {
   TSOpt          tsopt = (TSOpt)ptr;
