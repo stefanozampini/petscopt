37df2a44629c413a9efd08d4389a31bd2363753f
Stefano Zampini <stefano.zampini@gmail.com>
2019-01-28 09:35:05 +0300

    MFEMExtra: few utilities
    ComponentCoefficient: this evaluates one component of a VectorCoefficient
    DiagonalMatrixCoefficient: uses a vector coefficient as Matrix coefficient
    added a couple of utility routine for meshes too

--- include/mfemopt/mfemextra.hpp
+++ include/mfemopt/mfemextra.hpp
@@ -5,6 +5,7 @@
 
 #if defined(PETSCOPT_HAVE_MFEMOPT)
 #include <mfemopt/reducedfunctional.hpp>
+#include <mfem/fem/coefficient.hpp>
 #include <mfem/mesh/pmesh.hpp>
 #include <mfem/linalg/petsc.hpp>
 
@@ -12,6 +13,37 @@ namespace mfemopt
 {
 
 mfem::ParMesh* ParMeshTest(MPI_Comm,mfem::Mesh&);
+
+void MeshGetElementsTagged(mfem::Mesh*,const mfem::Array<int>&,mfem::Array<bool>&);
+void MeshGetElementsTagged(mfem::Mesh*,bool(*)(const mfem::Vector&),mfem::Array<bool>&);
+
+class ComponentCoefficient : public mfem::Coefficient
+{
+private:
+   mfem::VectorCoefficient *VQ;
+   int c;
+   mfem::Vector w;
+
+public:
+   ComponentCoefficient(mfem::VectorCoefficient&,int);
+   virtual double Eval(mfem::ElementTransformation&,
+                       const mfem::IntegrationPoint&);
+};
+
+class DiagonalMatrixCoefficient : public mfem::MatrixCoefficient
+{
+private:
+   mfem::VectorCoefficient *VQ;
+   mfem::Vector w;
+   bool own;
+
+public:
+   DiagonalMatrixCoefficient(mfem::VectorCoefficient*,bool=false);
+
+   virtual void Eval(mfem::DenseMatrix&,mfem::ElementTransformation&,
+                     const mfem::IntegrationPoint&);
+   virtual ~DiagonalMatrixCoefficient();
+};
 
 class PetscNonlinearSolverOpt : public mfem::PetscNonlinearSolver
 {
--- src/mfemopt/mfemextra.cpp
+++ src/mfemopt/mfemextra.cpp
@@ -20,6 +20,77 @@ ParMesh* ParMeshTest(MPI_Comm comm, Mesh &mesh)
    return pmesh;
 }
 
+void MeshGetElementsTagged(Mesh *mesh, bool (*tag_fn)(const Vector&),Array<bool>& tag)
+{
+   tag.SetSize(mesh->GetNE());
+   Vector pt(mesh->SpaceDimension());
+   for (int e = 0; e < mesh->GetNE(); e++)
+   {
+      mesh->GetElementTransformation(e)->Transform(
+         Geometries.GetCenter(mesh->GetElementBaseGeometry(e)), pt);
+      tag[e] = (*tag_fn)(pt);
+   }
+}
+
+void MeshGetElementsTagged(Mesh *mesh,const Array<int>& which_tag,Array<bool>& tag)
+{
+   tag.SetSize(mesh->GetNE());
+   for (int i = 0; i < mesh->GetNE(); i++)
+   {
+      bool ltag = false;
+      int eatt = mesh->GetAttribute(i);
+      for (int r = 0; r < which_tag.Size(); r++)
+      {
+         if (which_tag[r] == eatt)
+         {
+            ltag = true;
+            break;
+         }
+      }
+      tag[i] = ltag;
+   }
+}
+
+ComponentCoefficient::ComponentCoefficient(VectorCoefficient& _VQ, int _c)
+{
+   MFEM_VERIFY(_c > -1,"Invalid component " << _c );
+   MFEM_VERIFY(_c < _VQ.GetVDim(),"Component " << _c << " too large. Max " << _VQ.GetVDim());
+   time = 0.0;
+   VQ = &_VQ;
+   c  = _c;
+   w.SetSize(VQ->GetVDim());
+}
+
+double ComponentCoefficient::Eval(ElementTransformation &T,
+                                  const IntegrationPoint &ip)
+{
+   VQ->Eval(w,T,ip);
+   return w(c);
+}
+
+DiagonalMatrixCoefficient::DiagonalMatrixCoefficient(VectorCoefficient* _VQ, bool _own) : MatrixCoefficient(0) /* XXX the class have the explicit constructor */
+{
+   MFEM_VERIFY(_VQ,"Missing VectorCoefficient");
+   MFEM_VERIFY(_VQ->GetVDim() > 0,"Invalid dim " << _VQ->GetVDim());
+   VQ = _VQ;
+   own = _own;
+   w.SetSize(VQ->GetVDim());
+   height = width = VQ->GetVDim();
+   time = 0.0;
+}
+
+void DiagonalMatrixCoefficient::Eval(DenseMatrix &K, ElementTransformation &T,
+                                     const IntegrationPoint &ip)
+{
+   VQ->Eval(w,T,ip);
+   K.Diag(w.GetData(),w.Size());
+}
+
+DiagonalMatrixCoefficient::~DiagonalMatrixCoefficient()
+{
+  if (own) delete VQ;
+}
+
 PetscNonlinearSolverOpt::PetscNonlinearSolverOpt(MPI_Comm comm, ReducedFunctional &rf,
                          const std::string &prefix, bool obj) : PetscNonlinearSolver(comm,rf,prefix)
 {
